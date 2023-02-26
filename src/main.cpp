061658fb9ecd9b4f80db7f64a9fe7949cddc7c7f
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-02-25 20:04:03 -0600

    boilerplate python code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <pybind11/pybind11/h>
+#include <pybind11/pybind11.h>
 
 namespace py = pybind11;
 
---
+++ src/test.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append('../build')
+from mincut import *
+
+print(mincut(1, 2))
