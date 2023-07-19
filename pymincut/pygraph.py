0acd0457ffab0a8bd9f10ce1f4808dc6cdf86bb6
Vikram Ramavarapu <vikramr2@illinois.edu>
2023-07-19 16:11:59 -0500

    PAckaged python-mincut

--- pymincut/pygraph.py
+++ pymincut/pygraph.py
@@ -1,6 +1,7 @@
 import sys
-sys.path.append('../build')
+
 from mincut_wrapper import CGraph, MincutResult, mincut
+
 
 class PyGraph:
     ''' Wrapper for CGraph to allow for generic node and edge lists
@@ -10,11 +11,12 @@ class PyGraph:
     nodes: nodes can be of any type and do not need to be in order
     edges: values in edge list still need to be in the node list
     '''
+
     def __init__(self, nodes_=[], edges_=[]):
         self.nodes = nodes_
         self.edges = edges_
         self.compact_map = {k: v for v, k in enumerate(nodes_)}
-    
+
     def add_node(self, n):
         self.nodes.append(n)
 
@@ -24,11 +26,13 @@ class PyGraph:
         if v not in self.nodes:
             self.nodes.append(v)
         self.edges.append((u, v))
-    
+
     def as_CGraph(self):
         ''' Conversion to CGraph to be used for mincut computation '''
         cgraph_nodes = list(range(len(self.nodes)))
-        cgraph_edges = list(map(lambda t: (self.compact_map[t[0]], self.compact_map[t[1]]), self.edges))
+        cgraph_edges = list(
+            map(lambda t: (self.compact_map[t[0]], self.compact_map[t[1]]),
+                self.edges))
 
         return CGraph(cgraph_nodes, cgraph_edges)
 
